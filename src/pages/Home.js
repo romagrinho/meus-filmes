de7dbd4417104acd721e6d39349911316fcfd8b9
aluno <aluno@LAB.ETEC187>
2022-09-05 20:48:05 -0300

    Criando a tela Home

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import {} from 'react-native';
+
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet} from 'react-native';
+
+export default function Home(){
+    return(
+
+       <View>
+        <Text> Meus Filmes Fav</Text>
+        <text> Ola Romario, Boa noite!</text>
+       </View>     
+    );
+}
+
+//Criando os estilos do layout
+const style = StyleSheet.create({
+
+
+});
